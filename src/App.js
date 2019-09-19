30d45e508f3897e59cf68c5b7e97907cad67dac3
Lê Thái San <lethaisan191193@gmail.com>
2019-09-20 00:24:47 +0700

    Implement Login page (on-going) - change Login/Logout Form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Menu from './Components/Menu'
 import routes from './Route-config'
 
 function showRoute(routes) {
     let xRoutes
-    if (routes.length >0) {
-        xRoutes = routes.map((route,index)=>{
+    if (routes.length > 0) {
+        xRoutes = routes.map((route, index) => {
             return (
-                <Route 
-                    key={index} 
-                    exact={route.exact} 
-                    path={route.path} 
-                    component={route.component} 
+                <Route
+                    key={index}
+                    exact={route.exact}
+                    path={route.path}
+                    component={route.component}
                 />
             )
         })
@@ -31,6 +33,7 @@ function App() {
             <div className="mt-4">
                 {showRoute(routes)}
             </div>
+            <ToastContainer/>            
         </div>
     );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,96 @@
 import React, { Component } from 'react'
+import { toast } from 'react-toastify';
+
 
 export default class componentName extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            passWord: '',
+            isSigned: true
+        };
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = (event) => {
+        toast.info('👌 Wow so easy!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
+        event.preventDefault();
+    }
+
+    handleSubmitLogoutForm = (event) => {
+        this.setState({
+            isSigned: false
+        })
+        event.preventDefault();
+    }
+
+    checkLogin = (isSigned) => {
+        let xForm = null
+        const { userName, passWord } = this.state
+        if (!isSigned) {
+            xForm =
+                <form className="form-signin" onSubmit={this.handleSubmit}>
+                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                    <input
+                        type="text"
+                        id="inputEmail"
+                        className="form-control"
+                        placeholder="Email address"
+                        required
+                        name="userName"
+                        value={userName}
+                        onChange={this.handleInputChange}
+                    />
+                    <input
+                        type="password"
+                        id="inputPassword"
+                        className="form-control"
+                        placeholder="Password"
+                        required
+                        name="passWord"
+                        value={passWord}
+                        onChange={this.handleInputChange}
+                    />
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </form>
+        } else {
+            xForm =
+                <form className="form-signin" onSubmit={this.handleSubmitLogoutForm}>
+                    <h1 className="h3 mb-3 font-weight-normal">Hello</h1>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Logout</button>
+                </form>
+        }
+        return xForm
+    }
+
+
     render() {
+        const defaulUserName = 'ltsan'
+        const defaultPassword = '123456'
+        const { isSigned } = this.state
+
         return (
 
             <div className="row justify-content-md-center">
                 <div className="col-md-3 text-center">
-                    <form className="form-signin">
-                        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required />
-                        <label htmlFor="inputPassword" className="sr-only">Password</label>
-                        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                        <div className="checkbox mb-3">
-                            <label>
-                                <input type="checkbox" defaultValue="remember-me" /> Remember me
-                            </label>
-                        </div>
-                        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                    </form>
-
-
+                    {this.checkLogin(isSigned)}
                 </div>
             </div>
 
